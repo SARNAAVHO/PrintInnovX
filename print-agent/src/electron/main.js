343e957bc286b90f42ab582d6542c0f930ef0290
SARNAAVHO <piyush30052004@gmail.com>
2026-06-18 12:03:20 +0530

    agent_changed_8

--- print-agent/src/electron/main.js
+++ print-agent/src/electron/main.js
@@ -7,6 +7,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log("DOTENV:", dotenv.config());
+console.log("API:", process.env.API_BASE_URL);
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
